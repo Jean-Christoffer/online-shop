cb3910ba746a1bfeb7a061229d8b8e5cb43161f6
Jean Christoffer <christofferhalf6@gmail.com>
2024-03-25 10:46:44 +0100

    refactor: remove server folder (#9)

--- server/src/db/supabase.mjs
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl =  process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabaseDevKey = process.env.SUPABASE_DEV_KEY
-const supabase = createClient(supabaseUrl, supabaseDevKey)
-
-export default supabase
